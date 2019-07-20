dd257819ae4cdc87a38ace3ec8510ae1c30f987b
Stephen O'Sullivan <steveo@data-whsperers.com>
2019-07-20 08:36:00 -0700

    Doing some moving about

--- src/main/java/com/datawhisperers/restfulexample/SwaggerConfig.java
+++ src/main/java/com/datawhisperers/restfulexample/SwaggerConfig.java
@@ -5,12 +5,15 @@
  */
 package com.datawhisperers.restfulexample;
 
+import io.swagger.annotations.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -31,6 +34,16 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .build()
                 .useDefaultResponseMessages(false);
              
+    }
+    
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("restful example")
+                .description("\"Spring Boot REST API for restful example\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
     }
     
   @Override
